2e1236d076be6995b0039804c18b39460a4bb407
moriano <moriano@gmail.com>
2019-12-29 09:24:31 +1100

    Reorganized code in pong sample

---
+++ pong/ball.py
@@ -0,0 +1,37 @@
+import pygame
+from random import randint
+
+from pygame.sprite import Sprite
+from pygame import Surface
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class Ball(Sprite):
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # Draw the ball (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        self.velocity = [randint(4, 8), randint(-8, 8)]
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0] * 1.2
+        self.velocity[1] = randint(-8, 8)
--- pong/pong.py
+++ pong/pong.py
@@ -1,75 +1,14 @@
 # Import the pygame library and initialise the game engine
 import pygame
-from random import randint
+
+from sprites import Ball
+from sprites import Paddle
+
 pygame.init()
 
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
-
-class Ball(pygame.sprite.Sprite):
-
-    # This class represents a car. It derives from the "Sprite" class in Pygame.
-
-    def __init__(self, color, width, height):
-        # Call the parent class (Sprite) constructor
-        super().__init__()
-
-        # Pass in the color of the car, and its x and y position, width and height.
-        # Set the background color and set it to be transparent
-        self.image = pygame.Surface([width, height])
-        self.image.fill(BLACK)
-        self.image.set_colorkey(BLACK)
-
-        # Draw the ball (a rectangle!)
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
-
-        self.velocity = [randint(4,8),randint(-8,8)]
-
-        # Fetch the rectangle object that has the dimensions of the image.
-        self.rect = self.image.get_rect()
-
-    def update(self):
-        self.rect.x += self.velocity[0]
-        self.rect.y += self.velocity[1]
-
-    def bounce(self):
-        self.velocity[0] = -self.velocity[0] * 1.2
-        self.velocity[1] = randint(-8,8)
-
-
-class Paddle(pygame.sprite.Sprite):
-
-    #  This class represents a car. It derives from the "Sprite" class in Pygame.
-
-    def __init__(self, color, width, height):
-        # Call the parent class (Sprite) constructor
-        super().__init__()
-
-        # Pass in the color of the car, and its x and y position, width and height.
-        # Set the background color and set it to be transparent
-        self.image = pygame.Surface([width, height])
-        self.image.fill(BLACK)
-        self.image.set_colorkey(BLACK)
-
-        # Draw the paddle (a rectangle!)
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
-
-        # Fetch the rectangle object that has the dimensions of the image.
-        self.rect = self.image.get_rect()
-
-    def moveUp(self, pixels):
-        self.rect.y -= pixels
-        # Check that you are not going too far (off the screen)
-        if self.rect.y < 0:
-            self.rect.y = 0
-
-    def moveDown(self, pixels):
-        self.rect.y += pixels
-        # Check that you are not going too far (off the screen)
-        if self.rect.y > 400:
-            self.rect.y = 400
 
 
 # Open a new window
@@ -85,7 +24,7 @@ paddleB = Paddle(WHITE, 10, 120)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
 
-ball = Ball(WHITE,10,10)
+ball = Ball(WHITE, 10, 10)
 ball.rect.x = 345
 ball.rect.y = 195
 
@@ -126,17 +65,17 @@ while carryOn:
     # --- Game logic should go here
     all_sprites_list.update()
 
-    #Check if the ball is bouncing against any of the 4 walls:
-    if ball.rect.x>=690:
+    #  Check if the ball is bouncing against any of the 4 walls:
+    if ball.rect.x >= 690:
         ball.velocity[0] = -ball.velocity[0]
-    if ball.rect.x<=0:
+    if ball.rect.x <= 0:
         ball.velocity[0] = -ball.velocity[0]
-    if ball.rect.y>490:
+    if ball.rect.y > 490:
         ball.velocity[1] = -ball.velocity[1]
-    if ball.rect.y<0:
+    if ball.rect.y < 0:
         ball.velocity[1] = -ball.velocity[1]
 
-    #Detect collisions between the ball and the paddles
+    #  Detect collisions between the ball and the paddles
     if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
         ball.bounce()
 
