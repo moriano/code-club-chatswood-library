f68c97c879e151fa3b9eb063318b631483de847e
moriano <moriano@gmail.com>
2020-01-18 12:47:26 +1100

    Added functions

--- pong/sprites.py
+++ pong/sprites.py
@@ -1,11 +1,8 @@
 import pygame
 from random import randint
-
 from pygame.sprite import Sprite
 from pygame import Surface
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+from constants import BLACK
 
 
 class Paddle(Sprite):
@@ -26,13 +23,13 @@ class Paddle(Sprite):
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
 
-    def moveUp(self, pixels):
+    def move_up(self, pixels):
         self.rect.y -= pixels
         # Check that you are not going too far (off the screen)
         if self.rect.y < 0:
             self.rect.y = 0
 
-    def moveDown(self, pixels):
+    def move_down(self, pixels):
         self.rect.y += pixels
         # Check that you are not going too far (off the screen)
         if self.rect.y > 400:
@@ -54,16 +51,17 @@ class Ball(Sprite):
         # Draw the ball (a rectangle!)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
 
-        self.velocity = [randint(4, 8), randint(-8, 8)]
+        self.x_movement = randint(4, 8)
+        self.y_movement = randint(-8, 8)
 
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
 
     def update(self):
-        self.rect.x += self.velocity[0]
-        self.rect.y += self.velocity[1]
+        self.rect.x += self.x_movement
+        self.rect.y += self.y_movement
 
     def bounce(self):
-        self.velocity[0] = -self.velocity[0] * 1.2
-        self.velocity[1] = randint(-8, 8)
+        self.x_movement = -self.x_movement
+        self.y_movement = self.y_movement
 
